214d60678b8de90f26e1d29d5072efe2afa78d72
karthik4dev <karthikpnrao.97@gmail.com>
2026-04-18 23:33:29 +0530

    changes in password and adding MockitoTestClasses.java

--- src/test/java/com/karthikProjects/AuthServer/AuthServerApplicationTests.java
+++ src/test/java/com/karthikProjects/AuthServer/AuthServerApplicationTests.java
@@ -1,8 +1,9 @@
 package com.karthikProjects.AuthServer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-
+@TestPropertySource("/application.properties")
 @SpringBootTest
 class AuthServerApplicationTests {
 
